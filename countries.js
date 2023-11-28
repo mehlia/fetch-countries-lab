844f1ee186c3cf64084c018551b2cb0e3559119f
mehlia <mehliarahman@gmail.com>
2023-11-28 17:03:55 +0000

    mvp complete & works for every country

--- countries.js
+++ countries.js
@@ -3,32 +3,30 @@ const getCountryByName = async (countryName) => {
     // fetch data from REST countries API
     const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`)
     const data = await response.json();
-    console.log(data);
+    const country = data[0];
+    console.log(country);
 
     const displayCountry = document.getElementById("country-info");
 
-    // array to get all attributes
-    data.message.forEach((countryInfo) => {
          const name = document.createElement('p');
-         name.textContent = `Country Name: ${countryInfo.name}`;
+         name.innerText = `Country Name: ${country.name.common}`;
          
          const capital = document.createElement('p');
-         capital.textContent = `Capital: ${countryInfo.capital}`;
+         capital.innerText = `Capital: ${country.capital[0]}`;
          
-         const language = document.createElement('p');
-         language.textContent = `Language: ${countryInfo.language}`;
+         const languages = document.createElement('p');
+         const languagesArray = Object.values(country.languages);
+         const languagesString = languagesArray.join(", ");
+         languages.innerText = `Language: ${languagesString}`;
          
          const population = document.createElement('p');
-         population.textContent = `Population: ${countryInfo.population}`;
+         population.innerText = `Population: ${country.population}`;
          
          displayCountry.appendChild(name);
          displayCountry.appendChild(capital);
-         displayCountry.appendChild(language);
+         displayCountry.appendChild(languages);
          displayCountry.appendChild(population);
-        })
-
-        document.querySelector('body').appendChild(displayCountry);
-}
+    }
 
 // call the function with specific country 
-getCountryByName("bangladesh");
+getCountryByName("singapore");
