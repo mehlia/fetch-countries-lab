b885c9f85efe8dcdc03b1f329698f5d26b89eac1
mehlia <mehliarahman@gmail.com>
2023-11-28 16:01:04 +0000

    fetched specific country & attempted to display country attributes

--- countries.js
+++ countries.js
@@ -1,10 +1,34 @@
-function getCountryByName(countryName){
-    fetch("https://restcountries.com/v3.1/name/{countryName}")
-    .then(response => {
-        return response.json;
-    })
+const getCountryByName = async (countryName) => {
 
-    .then(data => {
-        console.log(data);
-    })
+    // fetch data from REST countries API
+    const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+    const data = await response.json();
+    console.log(data);
+
+    const displayCountry = document.getElementById("country-info");
+
+    // array to get all attributes
+    data.message.forEach((countryInfo) => {
+         const name = document.createElement('p');
+         name.textContent = `Country Name: ${countryInfo.name}`;
+         
+         const capital = document.createElement('p');
+         capital.textContent = `Capital: ${countryInfo.capital}`;
+         
+         const language = document.createElement('p');
+         language.textContent = `Language: ${countryInfo.language}`;
+         
+         const population = document.createElement('p');
+         population.textContent = `Population: ${countryInfo.population}`;
+         
+         displayCountry.appendChild(name);
+         displayCountry.appendChild(capital);
+         displayCountry.appendChild(language);
+         displayCountry.appendChild(population);
+        })
+
+        document.querySelector('body').appendChild(displayCountry);
 }
+
+// call the function with specific country 
+getCountryByName("bangladesh");
